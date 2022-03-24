40635217689a1e4366adf8bcf193593ea8e06d01
JonathasGit <jonathas-25@hotmail.com>
2022-03-24 18:41:15 -0300

    #14 código das aulas 17 e 18

---
+++ Fundamentos/inicio/comentario.js
@@ -0,0 +1,12 @@
+// Console.log é usado para exibir algo na tela do computador
+
+console.log('Passo 01');
+console.log('Passo 02'); // mais um exemplo de console.log
+
+
+
+/*
+*
+* Esse é um comentário de múltiplas linhas
+*/
+console.log('Passo 03');
