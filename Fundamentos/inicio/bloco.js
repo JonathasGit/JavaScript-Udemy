b7f7e39539d4c3f76764ec5da748c6cba06c3511
JonathasGit <jonathas-25@hotmail.com>
2022-03-24 19:09:28 -0300

    #14 aula 21 bloco de código

---
+++ Fundamentos/inicio/bloco.js
@@ -0,0 +1,16 @@
+{
+    console.log("Passo 1");
+    console.log("Passo 2");
+    console.log("Passo 2");
+}
+
+{
+    console.log("Passo 4");
+    console.log("Passo 5");
+}
+
+console.log("Fim");
+
+// bloco de código, serve para agrupar sentenças de código. 
+
+// PAstas - arquivos - sentença de código/blocos
