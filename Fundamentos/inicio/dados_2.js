964dbc6ee6c66a1b4498315d932edae9a6bba9e5
JonathasGit <jonathas-25@hotmail.com>
2022-03-30 22:39:53 -0300

    #14 aula 24

---
+++ Fundamentos/inicio/dados_2.js
@@ -0,0 +1,18 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+
+
+
+let precoComDesconto = (preco * (1 - desconto));
+
+console.log(preco * (1 - desconto));
+
+console.log(precoComDesconto + " Preço com desconto");
+console.log(19.9 * 0.6);
+
+let nome = "Caderno "; //String (texto) -> sequencia de simbolos
+let categoria =  "Papelaria"; 
+console.log(nome + categoria);
+console.log("Produto: " + nome + ", categoria " +  categoria + ", Preço: " + preco + ", Desconto: " + desconto);
+
