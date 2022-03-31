369d7b9b054eeb9b0b50149fe8cf8b99e6ae6cfa
JonathasGit <jonathas-25@hotmail.com>
2022-03-31 20:34:51 -0300

    #14 resposta do professor

--- Fundamentos/inicio/desafio_troca.js
+++ Fundamentos/inicio/desafio_troca.js
@@ -1,9 +1,16 @@
 let a = 7;
 let b = 94; 
+let temp = a;
 
 //trocar as variaveis
 
+//[a,b] = [b,a] troca mais avançada
 a = 94; 
 b = 7; 
 
+
 console.log(a, b);
+
+//a = b; 
+//b = temp;
+//console.log(a, b);
