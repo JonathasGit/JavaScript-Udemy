2e8b082b7382f5791e246702eea3fdafc16a9045
JonathasGit <jonathas-25@hotmail.com>
2022-04-21 22:40:10 -0300

    #15 aula 37 comentarios de codigo

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,19 @@
+//Comentários de código
+// Comentário de uma linha 
+console.log('Linhaa 1 ');
+
+/*
+ Comentário de 
+ múltiplas linhas
+*/
+
+console.log('Linhas 2');
+
+
+/* 
+* comentário de 
+* múltiplas linhas 
+* elegante
+*/
+
+console.log('Linhas 3');
