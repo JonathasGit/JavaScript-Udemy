df3880459c720599e7b4d4b254611ff1c121cdf4
JonathasGit <jonathas-25@hotmail.com>
2022-04-24 22:52:59 -0300

    #15 tipagem fraca

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer); //ver o tipo de variavel
+
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+ // evitar nome genericos e siglas
+
+ let valor = ''; 
+ let numero = 1;
+ let pqp = false // Produto Quimico Perigoso... kkkk
