eac58398c899fe8c8c3eedfedfcf2856d0e4f4a4
JonathasGit <jonathas-25@hotmail.com>
2022-04-30 23:42:16 -0300

    #51 Treinando função

---
+++ Praticando/funcaoSoma.js
@@ -0,0 +1,17 @@
+let a = 100;
+let b = 15; 
+let soma2 = soma(); 
+let subtrai1 = subtrai();
+
+// Aqui vai somar
+function soma () {
+    return a + b; 
+  
+}
+console.log(soma2)
+
+// subtração
+function subtrai(){
+    return a - b;
+}
+console.log(subtrai1);
