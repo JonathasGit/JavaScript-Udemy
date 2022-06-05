91d6f89c61d2ea01c4f4cdc622bedb6e00ead32e
JonathasGit <jonathas-25@hotmail.com>
2022-06-04 23:43:55 -0300

    #15 aulade numero 41

---
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+//FUnçõe estranhas que podem ser feitas no JS
+
+console.log(7/0); // retornar infinity 
+console.log(7 / 0.0001);
+console.log("10" / 2); //Não pode dar certo, string dividir para numero
+console.log("Show" * 2);
+console.log(0.1 + 0.7); // tipo da especificação do ponto flutuante
+//console.log(10.toString());
+console.log((10.345).toFixed(2));
