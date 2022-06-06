bcde3a4c5e3e3260af4751bcb278889c7271eac6
JonathasGit <jonathas-25@hotmail.com>
2022-06-05 22:33:54 -0300

    #15 terminando a aula

--- Fundamentos/math.js
+++ Fundamentos/math.js
@@ -1,5 +1,7 @@
 const raio = 5.6;
-const area = Math.PI * Math.pow(raio, 2);
+const area = Math.PI * Math.pow(raio, 2); 
+//Pi constante dentro de Math
+//pow - calcula a potencia 
 
 console.log(area)
 console.log(typeof Math)
