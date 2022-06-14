1600d60cf3fc08505bc7356670c11668527f9df2
JonathasGit <jonathas-25@hotmail.com>
2022-06-13 23:34:02 -0300

    #15 aula 43

---
+++ Fundamentos/strings.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r";
+
+console.log(escola.charAt(4)); // irá mostrar o indice R começando do C que é zero 
+console.log(escola.charAt(5)); // não irá mostrar erro, vai mostrar vazio.
+console.log(escola.charCodeAt(3)); // mostrar valor dele na tabela ASC ou Unicode
+console.log(escola.indexOf('3')); // index associado ao digito 3
+
+//Praticando
+console.log(escola.indexOf('C')); 
+console.log(escola.indexOf('o'));
+console.log(escola.indexOf('d'));
+console.log(escola.indexOf('3'));
+console.log(escola.indexOf('r'));
+
+
+
+
